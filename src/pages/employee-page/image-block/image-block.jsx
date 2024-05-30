0246fd3e7cdc7ef23817d94579982dc03c70271f
Sergey <sergey.sav.26666@mail.ru>
2024-05-30 16:54:04 +0300

    добавил onClick

--- src/pages/employee-page/image-block/image-block.jsx
+++ src/pages/employee-page/image-block/image-block.jsx
@@ -4,12 +4,38 @@ import styles from './image-block.module.scss';
 import { Slider } from '../../../components/slider/slider';
 
 export const ImageBlock = ({ data, isMobile }) => {
+    const handleDownloadVcf = () => {
+        const [surName, name, secondName] = data?.info.name.split(' ');
+
+        const text = [
+            'BEGIN:VCARD',
+            'VERSION:3.0',
+            `N:${surName};${name};${secondName}`,
+            `FN:${name} ${secondName} ${surName}`,
+            `TEL;CELL:${data?.items[0].name}`,
+            'END:VCARD',
+        ].join('\n');
+
+        const blob = new Blob([text], { type: 'text/vcard' });
+
+        const link = document.createElement('a');
+
+        link.href = window.URL.createObjectURL(blob);
+        link.setAttribute('download', 'SmartService.vcf');
+
+        document.body.appendChild(link);
+
+        link.click();
+
+        link.parentNode.removeChild(link);
+    };
+
     return (
         <div className={styles.wrapper}>
             <Slider data={data} isMobile={isMobile} />
-            <button className={styles.button}>добавить в контакты</button>
+            <button onClick={handleDownloadVcf} className={styles.button}>
+                добавить в контакты
+            </button>
         </div>
     );
 };
-
-//button для добавления в контакты, пока заглушка со стилями
