ef025e30948a2b2f36ae1b6ce7678366371ceaf9
Sergey <sergey.sav.26666@mail.ru>
2024-05-30 12:46:28 +0300

    пробую другой сервер 2

--- src/__data__/store/actions/actions.js
+++ src/__data__/store/actions/actions.js
@@ -4,7 +4,7 @@ export const fetchData = () => async (dispatch) => {
     try {
         dispatch(smartServiceSlice.actions.dataFetch());
 
-        const url = `https://mrsergeysavenko.github.io/server-for-smart-service/db.json`;
+        const url = `https://my-json-server.typicode.com/MrSergeySavenko/server-for-smart-service`;
 
         const response = await fetch(url);
         const data = await response.json();
