48057d0f8b4d20700e180074faffcd07d8080646
Sergey <sergey.sav.26666@mail.ru>
2024-05-30 13:24:39 +0300

    обратно к jsonserver

--- src/__data__/store/actions/actions.js
+++ src/__data__/store/actions/actions.js
@@ -4,7 +4,7 @@ export const fetchData = () => async (dispatch) => {
     try {
         dispatch(smartServiceSlice.actions.dataFetch());
 
-        const url = `http://localhost:3001/users`;
+        const url = `http://localhost:8000/items`;
 
         const response = await fetch(url);
         const data = await response.json();
--- src/index.js
+++ src/index.js
@@ -8,23 +8,22 @@ import { Router } from './components/Router';
 import { Provider } from 'react-redux';
 import { setupStore } from './__data__/store/store';
 
-async function enableMocking() {
-    if (process.env.NODE_ENV !== 'development') {
-        return;
-    }
+// async function enableMocking() {
+//     if (process.env.NODE_ENV !== 'development') {
+//         return;
+//     }
 
-    const { worker } = await import('./mocks/browser');
+//     const { worker } = await import('./mocks/browser');
 
-    return worker.start();
-}
+//     return worker.start();
+// }
 
-enableMocking().then(() => {
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-        <Provider store={setupStore()}>
-            <BrowserRouter>
-                <Router />
-            </BrowserRouter>
-        </Provider>
-    );
-});
+// enableMocking().then(() => {
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={setupStore()}>
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    </Provider>
+);
