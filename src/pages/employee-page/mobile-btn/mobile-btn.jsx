4090a9d2598de0f2f746695a14110aa458af9e8b
Sergey <sergey.sav.26666@mail.ru>
2024-05-30 16:46:03 +0300

    добавил alt

--- src/pages/employee-page/mobile-btn/mobile-btn.jsx
+++ src/pages/employee-page/mobile-btn/mobile-btn.jsx
@@ -6,14 +6,14 @@ export const MobileBtn = ({ src, children, onClick, type, phone }) => {
     if (type === 'link') {
         return (
             <a href={`tel:${phone}`} className={styles.link} onClick={onClick}>
-                <img src={src} className={styles.img} />
+                <img src={src} className={styles.img} alt='сохранить визитку' />
                 <p className={styles.text}>{children}</p>
             </a>
         );
     }
     return (
         <button className={styles.button} onClick={onClick}>
-            <img src={src} className={styles.img} />
+            <img src={src} className={styles.img} alt='позвонить' />
             <p className={styles.text}>{children}</p>
         </button>
     );
